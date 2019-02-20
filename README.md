049fc51ba0c01eaff91b91986250422ea5f3fc79
Ian Macdonald <ian@caliban.org>
2019-02-20 11:24:30 +0100

    Update to latest Magisk template and doc updates.

--- README.md
+++ README.md
@@ -2,13 +2,33 @@
 
 ## Description
 
-This Magisk module contains modified `libsecure_storage.so` libraries that allow Samsung Galaxy S9/S9+ (G960F/G965F), Tab S4 (T830/T835), and probably other rooted devices running Android 8.x to function without losing Bluetooth pairings after reboot.
+This Magisk module contains modified `libsecure_storage.so` libraries that allow rooted Samsung Galaxy S9/S9+ (G960F/G965F), Tab S4 (T830/T835) and probably other devices running Android 8.x (Oreo) to function without losing Bluetooth pairings after reboot.
 
-After installing this module, you may wish to manually edit `/system/etc/init/secure_storage_daemon.rc` and change the line that reads **start secure_storage** to **stop secure_storage**. This step is not required, but will prevent the secure storage daemon from running unnecessarily. This change cannot be applied by Magisk, because the file is read and acted upon early in the boot process before Magisk runs.
+After installing this module, you may wish to manually edit `/system/etc/init/secure_storage_daemon.rc` to change the line that reads:
 
-Use of this module on non-Oreo systems is untested. The problem it addresses may not exist on other versions of Android, and installing it on such devices may actually create problems.
+```
+start secure_storage
+```
+
+to:
+
+```
+stop secure_storage
+```
+
+Whilst not required, this extra step will prevent the secure storage daemon from running unnecessarily. This change cannot be dynamically applied by Magisk, because the file is read early in the boot process before Magisk runs.
+
+No files other than the ones provided by this module are required.
+
+The use of this module on non-Oreo systems is untested. Installing it on such devices may actually create issues whilst solving none.
 
 ## Changelog
+
+2018-02-20: v1.2
+
+- Update to latest Magisk template.
+- Minimum version of Magisk now v17.0.
+- Documentation updates.
 
 2018-09-06: v1.1
 
--- config.sh
+++ config.sh
@@ -1,33 +1,24 @@
 ##########################################################################################
 #
-# Magisk
+# Magisk Module Template Config Script
 # by topjohnwu
-# 
-# This is a template zip for developers
 #
 ##########################################################################################
 ##########################################################################################
-# 
+#
 # Instructions:
-# 
+#
 # 1. Place your files into system folder (delete the placeholder file)
 # 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (common/config.sh)
-# 4. For advanced features, add shell commands into the script files under common:
-#    post-fs-data.sh, service.sh
-# 5. For changing props, add your additional/modified props into common/system.prop
-# 
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
+#
 ##########################################################################################
 
 ##########################################################################################
-# Defines
+# Configs
 ##########################################################################################
-
-# NOTE: This part has to be adjusted to fit your own needs
-
-# This will be the folder name under /magisk
-# This should also be the same as the id in your module.prop to prevent confusion
-MODID=libsecure_storage
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -49,9 +40,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "********************************"
-  ui_print "    Custom libsecure_storage    "
-  ui_print "********************************"
+  ui_print "*******************************"
+  ui_print "  libsecure_storage companion  "
+  ui_print "*******************************"
 }
 
 ##########################################################################################
@@ -59,10 +50,8 @@ print_modname() {
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
-# By default Magisk will merge your files with the original system
-# Directories listed here however, will be directly mounted to the correspond directory in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
 
-# You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
 REPLACE="
 /system/app/Youtube
@@ -71,24 +60,20 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list here, it will overwrite the example
+# Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
-REPLACE=""
+REPLACE="
+"
 
 ##########################################################################################
 # Permissions
 ##########################################################################################
 
-# NOTE: This part has to be adjusted to fit your own needs
-
 set_permissions() {
-  # Default permissions, don't remove them
-  set_perm_recursive  $MODPATH  0  0  0755  0644
-
   # Only some special files require specific permissions
   # The default permissions should be good enough for most cases
 
-  # Some templates if you have no idea what to do:
+  # Here are some examples for the set_perm functions:
 
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
   # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
@@ -97,4 +82,18 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
 }
+
+##########################################################################################
+# Custom Functions
+##########################################################################################
+
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
+
